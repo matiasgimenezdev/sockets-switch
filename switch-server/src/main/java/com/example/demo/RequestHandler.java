41b063c36f63e998b5aa80dc9bf68dc43b3ce8fa
Matias Gimenez <matiasgimenez.dev@gmail.com>
2023-10-29 17:56:34 -0300

    creation of  tables on both database

--- switch-server/src/main/java/com/example/demo/RequestHandler.java
+++ switch-server/src/main/java/com/example/demo/RequestHandler.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import com.google.gson.Gson;
 import java.io.*;
 import java.net.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 class RequestHandler implements Runnable {
 
@@ -13,29 +17,38 @@ class RequestHandler implements Runnable {
   }
 
   public void run() {
+    StringBuilder stringBuilder;
+    System.out.println("Servidor switch iniciado en el puerto 5000.");
+
+    // Realiza operaciones en la base de datos aquí
     // Parámetros de conexión postgres
     String postgresIp = "192.168.3.4";
     String postgresPort = "5432";
     String postgresUsername = "admin";
     String postgresPassword = "masterkey";
-    String postgresUrl =
-      "jdbc:postgresql://" +
-      postgresIp +
-      ":" +
-      postgresPort +
-      "/personal-switch";
+    stringBuilder =
+      new StringBuilder("jdbc:postgresql://")
+        .append(postgresIp)
+        .append(":")
+        .append(postgresPort)
+        .append("/personal");
+    String postgresUrl = stringBuilder.toString();
 
     // Parámetros de conexión firebird
     String firebirdIp = "192.168.3.3";
     String firebirdPort = "3050";
     String firebirdUsername = "sysdba";
     String firebirdPassword = "masterkey";
-    String firebirdUrl =
-      "jdbc:firebird://" +
-      firebirdIp +
-      ":" +
-      firebirdPort +
-      "/firebird/data/facturacion-switch.fdb";
+    stringBuilder =
+      new StringBuilder("jdbc:firebirdsql:")
+        .append(firebirdIp)
+        .append("/")
+        .append(firebirdPort)
+        .append(":/firebird/data/facturacion.fdb");
+    String firebirdUrl = stringBuilder.toString();
+
+    Connection connection;
+    Statement statement;
 
     try {
       // Leo la peticion desde el socket del cliente
@@ -50,6 +63,24 @@ class RequestHandler implements Runnable {
       Gson gson = new Gson();
       Query query = gson.fromJson(json, Query.class);
 
+      connection =
+        DriverManager.getConnection(
+          firebirdUrl,
+          firebirdUsername,
+          firebirdPassword
+        );
+      statement = connection.createStatement();
+      System.out.println("Conexión exitosa a la base de datos FIREBIRD.");
+
+      connection =
+        DriverManager.getConnection(
+          postgresUrl,
+          postgresUsername,
+          postgresPassword
+        );
+
+      System.out.println("Conexión exitosa a la base de datos PostgreSQL.");
+
       // Devuelvo resultado del query
       PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
       out.println("Resultado del query: " + query.getQuery());
@@ -57,10 +88,16 @@ class RequestHandler implements Runnable {
       // Cierro streams y socket
       in.close();
       out.close();
+      connection.close();
+      statement.close();
       clientSocket.close();
     } catch (IOException e) {
       System.out.println(
         "Error al manejar la conexión del cliente: " + e.getMessage()
+      );
+    } catch (SQLException e) {
+      System.err.println(
+        "Error al conectar a la base de datos FIREBIRD: " + e.getMessage()
       );
     }
   }
--- switch-server/src/main/java/com/example/demo/SwitchServer.java
+++ switch-server/src/main/java/com/example/demo/SwitchServer.java
@@ -1,15 +1,150 @@
 package com.example.demo;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SwitchServer {
 
   public void run() {
     ServerSocket socket = null;
+    StringBuilder stringBuilder;
     try {
       socket = new ServerSocket(5000);
       System.out.println("Servidor switch iniciado en el puerto 5000.");
+
+      // Realiza operaciones en la base de datos aquí
+      // Parámetros de conexión postgres
+      String postgresIp = "192.168.3.4";
+      String postgresPort = "5432";
+      String postgresUsername = "admin";
+      String postgresPassword = "masterkey";
+      stringBuilder =
+        new StringBuilder("jdbc:postgresql://")
+          .append(postgresIp)
+          .append(":")
+          .append(postgresPort)
+          .append("/personal");
+      String postgresUrl = stringBuilder.toString();
+
+      // Parámetros de conexión firebird
+      String firebirdIp = "192.168.3.3";
+      String firebirdPort = "3050";
+      String firebirdUsername = "sysdba";
+      String firebirdPassword = "masterkey";
+      stringBuilder =
+        new StringBuilder("jdbc:firebirdsql:")
+          .append(firebirdIp)
+          .append("/")
+          .append(firebirdPort)
+          .append(":/firebird/data/facturacion.fdb");
+      String firebirdUrl = stringBuilder.toString();
+
+      try {
+        Thread.sleep(8000); // Pausa la ejecución para esperar a que los contenedores esten corriendo.
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+
+      Connection connection;
+      Statement statement;
+      String query;
+
+      try {
+        connection =
+          DriverManager.getConnection(
+            firebirdUrl,
+            firebirdUsername,
+            firebirdPassword
+          );
+
+        statement = connection.createStatement();
+        System.out.println("Conexión exitosa a la base de datos FIREBIRD.");
+        query =
+          """
+          CREATE TABLE FACTURA
+          (
+            NUMERO INTEGER NOT NULL,
+            FECHA DATE NOT NULL,
+            MONTO DOUBLE PRECISION DEFAULT 0.0,
+            CONSTRAINT PK_FACTURA PRIMARY KEY (NUMERO)
+          );
+        """;
+
+        statement.execute(query);
+        query =
+          """
+          CREATE TABLE PRODUCTO
+          (
+            CODIGO INTEGER NOT NULL,
+            DESCRIPCION VARCHAR(100) NOT NULL,
+            STOCK INTEGER DEFAULT 0 NOT NULL,
+            PRECIO DOUBLE PRECISION DEFAULT 0 NOT NULL,
+            CONSTRAINT PK_PRODUCTO PRIMARY KEY (CODIGO)          
+          );
+          """;
+        statement.execute(query);
+        query =
+          """
+          CREATE TABLE DETALLE
+          (
+            NUMERO INTEGER NOT NULL,
+            CODIGO INTEGER NOT NULL,
+            CANTIDAD INTEGER DEFAULT 1 NOT NULL,
+            PRECIO DOUBLE PRECISION DEFAULT 0 NOT NULL,
+            SUBTOTAL DOUBLE PRECISION,
+            CONSTRAINT PK_DETALLE PRIMARY KEY (NUMERO,CODIGO),
+            CONSTRAINT FK_DETALLE_PRODUCTO FOREIGN KEY (CODIGO) REFERENCES PRODUCTO,
+            CONSTRAINT FK_DETALLE_FACTURA FOREIGN KEY (NUMERO) REFERENCES FACTURA
+          );   
+          """;
+        statement.execute(query);
+
+        System.out.println("Tablas en FACTURACION creadas con éxito.");
+
+        statement.close();
+        connection.close();
+      } catch (SQLException e) {
+        System.err.println(
+          "Error al conectar a la base de datos FIREBIRD: " + e.getMessage()
+        );
+      }
+
+      try {
+        connection =
+          DriverManager.getConnection(
+            postgresUrl,
+            postgresUsername,
+            postgresPassword
+          );
+
+        System.out.println("Conexión exitosa a la base de datos PostgreSQL.");
+
+        query =
+          """
+          CREATE TABLE EMPLEADO (
+            ID serial PRIMARY KEY,
+            NOMBRE VARCHAR(120) NOT  NULL,
+            APELLIDO VARCHAR(120) NOT  NULL,
+            SALARIO NUMERIC(10, 2) DEFAULT 0
+          );
+        """;
+
+        statement = connection.createStatement();
+        statement.execute(query);
+        statement.close();
+        connection.close();
+
+        System.out.println("Tablas en PERSONAL creadas con éxito.");
+      } catch (SQLException e) {
+        System.err.println(
+          "Error al conectar a la base de datos POSTGRES: " + e.getMessage()
+        );
+      }
 
       while (true) {
         Socket clientSocket = socket.accept();
